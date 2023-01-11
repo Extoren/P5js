b834a5dabf593bef3de81b6ced61d2015491592a
Extoren <sffjo.aarre@gmail.com>
2023-01-11 15:26:38 +0100

    First Upload

--- main.js
+++ main.js
@@ -1,12 +1,94 @@
-let firstName; 
-let lastName;
+function loaderSpinner() {
+    $(window).load(function() {
+        var loader = $('.loader');
+var wHeight = $(window).height();
+var wWidth = $(window).width();
+var i = 0;
+/*Center loader on half screen */
+loader.css({
+    top: wHeight / 2 - 2.5,
+    left: wWidth / 2 - 200
+ })
+      
+  do{
+    loader.animate({
+      width: i
+    },10)
+    i+=3;
+  } while(i <= 400)
+  if(i === 402){
+    loader.animate({
+      left: 0,
+      width: '100%'
+    })
+    loader.animate({
+      top: '0',
+      height: '100vh'
+    })
+  }
+      
+      /* This line hide loader and show content */
+      setTimeout(function(){
+        $('.content').fadeIn("slow");
+        (loader).fadeOut("fast");
+        /*Set time in milisec */
+      },3500);
+    });
 
-function displayMessage() {
-  
-firstName = document.getElementById("first").value;
-  
-lastName = document.getElementById("last").value;
-
-document.getElementById("message").innerHTML = "Congrats on your first experience on this website " + firstName + " " + lastName + "!";
 }
 
+loaderSpinner();
+
+
+
+
+
+
+
+
+$(function() {
+  function Clock() {
+    var theDate = new Date();
+    var t = theDate.getHours() + ":" + theDate.getMinutes() + ":" + theDate.getSeconds();
+    $('#time').text(t);
+  }
+  setInterval(Clock, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  // Setup our variables
+  var cH = $('#crosshair-h'),
+      cV = $('#crosshair-v');
+  
+  $(this).on('mousemove touchmove', function(e) {
+    var x = e.pageX - 1;
+    var y = e.pageY - 1;
+    cH.css('top', e.pageY);
+    cV.css('left', e.pageX);
+    
+    $('#mousepos').css({
+      top: e.pageY + 'px',
+      left: e.pageX + 'px'
+    }, 800);
+    $('#mousepos').text( "X: " + x + "px, Y: " + y + "px");
+    e.stopPropagation();
+  });
+  
+  // Anchor Hover Effects
+  $("a").hover(function() {
+    $(".hair").stop().css({borderColor: "#fff"}, 800)},
+     function() {
+    $(".hair").stop().css({borderColor: "black"},800)
+  });
+  e.stopPropagation();
+});
